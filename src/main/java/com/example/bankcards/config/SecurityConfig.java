593dbddae2395b43b966b882644afa1c2071a4ef
SamuelichSam <samuelsamuelichsemenov@yandex.ru>
2025-09-17 15:43:36 +0300

    Bank_rest ver. 1.0.0

--- src/main/java/com/example/bankcards/config/SecurityConfig.java
+++ src/main/java/com/example/bankcards/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
+                                "/v3/api-docs.yaml",
                                 "/swagger-resources/**",
                                 "/webjars/**"
                         ).permitAll()
